a708193affc2abfcdc7931b41ddb9f78284a240b
tekrat1 <tekrat10@gmail.com>
2026-07-18 12:14:30 +0530

    fix: resolve bugs and fix 404 redirect issue

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "ik.imagekit.io",
-  //       port: "",
-  //     },
-  //   ],
-  // },
   serverExternalPackages: ["pdf-parse"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
